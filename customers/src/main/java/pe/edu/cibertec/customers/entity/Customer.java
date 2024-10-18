e972f5e21bac0c041ca03d7ee2ead8734849324e
VieriElCrack1 <santosvieri9@gmail.com>
2024-10-17 21:47:32 -0500

    configurando kafka

---
+++ customers/src/main/java/pe/edu/cibertec/customers/entity/Customer.java
@@ -0,0 +1,10 @@
+package pe.edu.cibertec.customers.entity;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private Integer id;
+    private String name;
+    private String email;
+}
---
+++ customers/src/main/java/pe/edu/cibertec/customers/events/EventType.java
@@ -0,0 +1,5 @@
+package pe.edu.cibertec.customers.events;
+
+public enum EventType {
+    CREATED, UPDATED, DELETED
+}
