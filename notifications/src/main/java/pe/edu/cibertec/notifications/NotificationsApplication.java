82f7e3e0dc0095c7edd726280a1ed672b709ac93
VieriElCrack1 <santosvieri9@gmail.com>
2024-10-17 21:21:41 -0500

    iniciando kafka

---
+++ notifications/src/main/java/pe/edu/cibertec/notifications/NotificationsApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsApplication.class, args);
+	}
+
+}
